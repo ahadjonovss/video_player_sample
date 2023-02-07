aafd5c05649d1ca1a62a2ba80540fb13ff341f7a
Samandar <samandarahadjonov@gmail.com>
2023-02-07 10:19:40 +0500

    video player added

--- lib/ui/single_video/single_video_page.dart
+++ lib/ui/single_video/single_video_page.dart
@@ -1,12 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:video_player/video_player.dart';
 
-class SingleVideoPage extends StatelessWidget {
+class SingleVideoPage extends StatefulWidget {
   const SingleVideoPage({Key? key}) : super(key: key);
+
+  @override
+  State<SingleVideoPage> createState() => _SingleVideoPageState();
+}
+
+class _SingleVideoPageState extends State<SingleVideoPage> {
+  late VideoPlayerController _controller;
+  @override
+  void initState() {
+    super.initState();
+    _controller = VideoPlayerController.asset(
+        "1.mp4")
+      ..initialize().then((_) {
+        setState(() {});
+      });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Video Player"),),
+      appBar: AppBar(title: const Text("Video Player"),),
+      body: Column(
+        children: [
+          _controller.value.isInitialized
+              ? AspectRatio(
+            aspectRatio: _controller.value.aspectRatio,
+            child: VideoPlayer(_controller),
+          )
+              : Container(),
+
+        ],
+      ),
     );
   }
 }
